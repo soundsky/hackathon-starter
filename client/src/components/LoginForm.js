8827a919817f8578b23271e8a6e640ab5ef5faa5
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-02 15:36:05 -0500

    updated the login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 const LoginForm = ({ status }) => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChanges = event => {
+        setUsers({ ...users, [event.target.email]: event.trget.value });
+    };
+
+    const submitForm = event => {
+        event.preventDefault();
+
+        status(users);
+
+        setUsers({ email: "", password: "" });
+    };
+    console.log("user state", users);
 
 
     useEffect(() => {
@@ -12,18 +28,39 @@ const LoginForm = ({ status }) => {
 
     return (
         <div>
-            <form>
-                <label>
-                    Email:
-                    <input type="email" />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" />
-                </label>
+            <form onSubmit={submitForm}>
+                <label htmlFor="name">Email:</label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    onChange={handleChanges}
+                    value={users.email}
+                />
+                <label htmlFor="name">Password:</label>
+                <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={handleChanges}
+                    value={users.password}
+                />
+
             </form>
         </div>
     )
 }
+
+// handleSubmit(values, { setStatus, restForm }) {
+//     console.log("submitting", values);
+//     axios
+//         .post('/login', values)
+//         .then(res => {
+//             console.log("success", res);
+//             setStatus(res.data);
+//             resetForm();
+//         })
+//         .catch(err => console.log(err.res));
+// }
 
 export default LoginForm;
