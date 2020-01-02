e9b093e20ca6225a632a15ef973c4a75490e6068
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-02 14:45:20 -0500

    added components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios';
+import SignupForm from './components/SignupForm';
+import LoginForm from './components/LoginForm';
 
 function App() {
   useEffect(() => {
@@ -19,20 +21,8 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignupForm />
+      <LoginForm />
     </div>
   );
 }
