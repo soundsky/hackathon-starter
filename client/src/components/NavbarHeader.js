7721e1db9a3015f9dd34e175caf7039e9bfb10e4
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-02 23:37:59 -0500

    took the search field out of the collapse menu button

--- client/src/components/NavbarHeader.js
+++ client/src/components/NavbarHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import { NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav, Form, FormControl, FormGroup, Button } from 'react-bootstrap';
+// import { NavLink } from 'reactstrap';
+// import { Link } from 'react-router-dom';
 // import NavbarToggler from 'reactstrap/lib/NavbarToggler';
 
 
@@ -11,19 +11,26 @@ export default function NavbarHeader() {
         <div>
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand href="/">Soundsky</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <FormGroup>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                </FormGroup>{' '}
+
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/signup">Signup</Nav.Link>
                         <Nav.Link href="/login">Login</Nav.Link>
-
                     </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
                 </Navbar.Collapse>
+                <Form inline>
+                    <FormGroup>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    </FormGroup>{' '}
+                    <FormGroup>
+                        <Button variant="outline-success">Search</Button>
+                    </FormGroup>{' '}
+
+                </Form>
             </Navbar>
         </div>
     )
